1fc138b8b6cb0f8faedd9e96dd05c53910e1f567
Tim Siegler <Tim.Siegler@fincon.eu>
2022-12-10 18:12:16 +0100

    Refactored main entry

--- src/main.ts
+++ src/main.ts
@@ -11,9 +11,12 @@ const app: express.Application = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/hello', helloWorld);
-app.use('/doc', apiDoc);
+const apiRouter: express.Router = express.Router();
+apiRouter.use('/hello', helloWorld);
+
+app.use('/api', apiRouter);
 app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument, { explorer: false }));
+app.use('/doc', apiDoc);
 
 app.listen(config.port, () => {
     console.log(`it's alive on http://localhost:${config.port}`);
